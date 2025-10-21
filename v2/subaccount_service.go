70cafc30405be75383b9472259cb74bb153d41f7
sc0Vu <alk03073135@gmail.com>
2025-10-21 21:27:50 +0800

    fix(subaccount): subUserId should be int64

--- v2/subaccount_service.go
+++ v2/subaccount_service.go
@@ -332,7 +332,7 @@ type SubAccountList struct {
 }
 
 type SubAccount struct {
-	SubUserID                   string `json:"subUserId"`
+	SubUserID                   int64  `json:"subUserId"`
 	Email                       string `json:"email"`
 	Remark                      string `json:"remark"`
 	IsFreeze                    bool   `json:"isFreeze"`
