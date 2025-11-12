4cf2a73c5d0ba6f2571dd4bd9f043176fdde04e6
0x0001 <2239315+0x0001@users.noreply.github.com>
2025-11-12 22:49:43 +0800

    perf(types): update weights and order counts using atomic operations
    Avoid data contention problems during concurrent access

--- v2/common/types.go
+++ v2/common/types.go
@@ -3,6 +3,7 @@ package common
 import (
 	"net/http"
 	"strconv"
+	"sync/atomic"
 )
 
 type UsedWeight struct {
@@ -14,13 +15,13 @@ func (u *UsedWeight) UpdateByHeader(header http.Header) {
 	usedWeight := header.Get("X-Mbx-Used-Weight")
 	if usedWeight != "" {
 		if used, err := strconv.ParseInt(usedWeight, 10, 64); err == nil {
-			u.Used = used
+			atomic.StoreInt64(&u.Used, used)
 		}
 	}
 	usedWeight1M := header.Get("X-Mbx-Used-Weight-1m")
 	if usedWeight1M != "" {
 		if used, err := strconv.ParseInt(usedWeight1M, 10, 64); err == nil {
-			u.Used1M = used
+			atomic.StoreInt64(&u.Used1M, used)
 		}
 	}
 }
@@ -34,14 +35,14 @@ func (o *OrderCount) UpdateByHeader(header http.Header) {
 	orderCount10s := header.Get("X-Mbx-Order-Count-10s")
 	if orderCount10s != "" {
 		if count, err := strconv.ParseInt(orderCount10s, 10, 64); err == nil {
-			o.Count10s = count
+			atomic.StoreInt64(&o.Count10s, count)
 		}
 	}
 
 	orderCount1d := header.Get("X-Mbx-Order-Count-1d")
 	if orderCount1d != "" {
 		if count, err := strconv.ParseInt(orderCount1d, 10, 64); err == nil {
-			o.Count1d = count
+			atomic.StoreInt64(&o.Count1d, count)
 		}
 	}
 }
